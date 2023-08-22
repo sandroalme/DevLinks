ffb877e4d867e009bd3e45186d06b5aebf23fb7a
sandroalme <111318914+sandroalme@users.noreply.github.com>
2023-08-21 23:38:26 -0300

    consertando bug na foto de perfil

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
   
     if (html.classList.contains("light")) {
-      img.setAttribute("src", "./assets/avatar-light.jpg")
+      img.setAttribute("src", "../assets/avatar-light.jpg")
     } else {
-      img.setAttribute("src", "./assets/avatar.jpg")
+      img.setAttribute("src", "../assets/avatar.jpg")
     }
   }
